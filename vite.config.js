0638501aa77263df7b2e596b0700a97b49e8f1e3
Hendrizal <hendri.zzzzz@gmail.com>
2026-04-21 11:22:38 +0800

    Perbaikan limit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/kanwil-app/", // ⬅️ WAJIB
+  build: {
+    chunkSizeWarningLimit: 1000, // Menghilangkan warning untuk file di bawah 1MB
+    rollupOptions: {
+      output: {
+        // Memecah bundle menjadi file-file kecil
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("jspdf")) return "vendor-pdf"; // Khusus library PDF
+            if (id.includes("react")) return "vendor-react"; // Khusus React
+            if (id.includes("@fortawesome")) return "vendor-icons"; // Khusus Icon
+            return "vendor"; // Sisa library lainnya
+          }
+        },
+      },
+    },
+  },
 })
